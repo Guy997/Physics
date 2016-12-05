9ce5536901162e0a15997ca67b852890d9a3a6b9
Sam Schwartz <schwartz.sam370@gmail.com>
2016-12-05 12:33:51 -0700

    Added in Work

--- Physics/src/kinematics/Work.java
+++ Physics/src/kinematics/Work.java
@@ -1,5 +1,49 @@
 package kinematics;
 
 public class Work {
-
+	
+	private static double work;
+	
+	public Work(double f, double d, double energy_init, double energy){
+		solve(f, d, energy_init, energy);
+	}
+	
+	static double findWorkForce(double f, double d){
+		work = f * d;
+		return work;
+	}
+	static double findWorkEnergy(double energy_init, double energy, double energyTotal){
+		if(energyTotal != 0){
+			work = energyTotal;
+			return work;
+		}
+		else{
+			energyTotal = energy - energy_init;
+			work = energyTotal;
+			return work;
+		}
+	}
+	
+	static double findEnergy(double f, double d, double energy, double energy_init, double energyTotal){
+		
+	}
+	static double findForce(double d, double energy, double energy_init, double energyTotal){
+		double force;
+		findWorkEnergy(energy_init, energy, energyTotal);
+		force = work / d;
+		return force;
+	}
+	static double findDist(double energy_init, double energy, double energyTotal, double f){
+		double dist;
+		findWorkEnergy(energy_init, energy, energyTotal);
+		dist = work / f;
+		return dist;
+	}
+	public static void solve(double f, double d, double energy_init, double energy, double energyTotal){
+		System.out.println("Work = " + findWorkForce(f, d));
+		System.out.println("Work = " + findWorkEnergy(energy_init, energy, energyTotal));
+		System.out.println("Distance = " + findDist(energy_init, energy, energyTotal, f));
+		System.out.println("Force = " + findForce(d, energy, energy_init, energyTotal));
+	}
+	
 }
--- Physics/src/main/PhysicsDriver.java
+++ Physics/src/main/PhysicsDriver.java
@@ -1,14 +1,15 @@
 package main;
 
-import kinematics.Energies;
 import kinematics.EnergiesRevised;
+import kinematics.Work;
 
 public class PhysicsDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EnergiesRevised.solve(2, false, true, true, 0, 2, 0, 3.2, 475, 0, 0);
+		EnergiesRevised.solve(1, false, true, true, 0, 2, 0, 3.2, 475, 0, 0);
 		//System.out.println(Energies.UG_);
+		Work.solve(0, 26.2, 0, 0, 354);
 	}
 
 }
